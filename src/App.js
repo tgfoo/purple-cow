ea346596637b8f85e6f23c25761423df029e0520
Tim Hill <timhill29@gmail.com>
2022-04-03 15:04:05 -0400

    Adds basic styling

--- src/App.js
+++ src/App.js
@@ -7,6 +7,10 @@ function App() {
       <header className="App-header">
         <h1>Project Purple Cow</h1>
       </header>
+      <section className="content">
+        <p className="button-count">142,344 Clicks</p>
+        <button>Click Me</button>
+      </section>
     </div>
   );
 }
